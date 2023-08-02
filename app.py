fa662cf633a88a75c86311468b67845bc121bc84
ss950 <meen79508@gmail.com>
2023-08-02 21:18:40 +0300

    V2

--- app.py
+++ app.py
@@ -20,12 +20,6 @@ mail = Mail(app)
 def home():
     return render_template('index.html')
 
-
-@app.route('/about')
-def about():
-    return render_template('about.html')
-
-
 @app.route('/portfolio')
 def portfolio():
     return render_template('portfolio.html')
@@ -36,55 +30,23 @@ def projects():
     return render_template('projects.html')
 
 
-# @app.route('/contact', methods=['GET', 'POST'])
-# def contact():
-#     if request.method == 'POST':
-#         name = request.form['name']
-#         email = request.form['email']
-#         message = request.form['message']
-#
-#         msg = Message(
-#             subject='New Message from Portfolio',
-#             sender=email,
-#             recipients=['your-email-address']
-#         )
-#         msg.body = f"From: {name}\nEmail: {email}\nMessage: {message}"
-#         mail.send(msg)
-#
-#         return redirect('/thank-you')
-#     return render_template('contact.html')
-def is_valid_email(email):
-    """
-    Check if an email address is in a valid format using regular expressions.
-    """
-    # Regular expression to match the email pattern
-    pattern = '^[\w]{1,}[\w.+-]{0,}@[\w-]{2,}([.][a-zA-Z]{2,}|[.][\w-]{2,}[.][a-zA-Z]{2,})$'
-
-    # Use re.match() to match the pattern against the email address
-    match = re.match(pattern, email)
-
-    # Return True if there is a match, False otherwise
-    return bool(match)
-
-@app.route('/contact', methods=['POST'])
+@app.route('/contact', methods=['POST'])  # Make sure to specify POST here
 def contact():
-    name = request.form['name']
-    email = request.form['email']
-    subject = request.form['subject']
-    message = request.form['message']
+    # Process the form data here
+    fullname = request.form.get('fullname')
+    email = request.form.get('email')
+    message = request.form.get('message')
 
-    # check if all fields are filled
-    if not name or not email or not subject or not message:
-        return jsonify({'message': 'error', 'error': 'Please fill out all fields.'})
-    # send the message
-    # ...
-    elif not is_valid_email(email):
-        return jsonify({'message': 'error', 'error': 'Please enter a valid email.'})
-
-    print("3rd ", name, email, subject, message)
-
-    return jsonify({'message': 'success', 'success': 'Your message was sent successfully.'})
-
+    # Send email using Flask-Mail
+    msg = Message('New Contact Form Submission', recipients=['recipient@example.com'])
+    msg.body = f"Full Name: {fullname}\nEmail: {email}\nMessage: {message}"
+    try:
+        mail.send(msg)
+        success = True
+    except Exception as e:
+        print(e)
+        success = False
+    return jsonify({'success': success})
 
 @app.route('/thank-you')
 def thank_you():
@@ -92,4 +54,4 @@ def thank_you():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
